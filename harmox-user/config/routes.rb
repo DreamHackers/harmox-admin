85e6843cbc0e0c88f974e696c6939b9f38c87083
Gaddict <ksano6253@gmail.com>
2015-06-11 19:58:33 +0900

    Modify routes

--- harmox-user/config/routes.rb
+++ harmox-user/config/routes.rb
@@ -1,8 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  #  , skip: [:sessions]
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }, skip: [:sessions, :registrations]
+
+
   get 'home/index'
 
   get 'home/show'
 
   root to: "home#index"
+
+  devise_scope :user do
+    # sessions
+    get 'signin' => 'devise/sessions#new', as: :new_user_session
+    post 'signin' => 'devise/sessions#create', as: :user_session
+    get 'signout' => 'devise/sessions#destroy', as: :destroy_user_session
+
+    # registrations
+    get 'cancel' => 'devise/registrations#cancel', as: :cancel_user_registration
+    post ':username' => 'devise/registrations#create', as: :user_registration
+    get 'signup' => 'devise/registrations#new', as: :new_user_registration
+    get ':username/edit' => 'devise/registrations#edit', as: :edit_user_registration
+    patch ':username' => 'devise/registrations#update'
+    put ':username' => 'devise/registrations#update'
+    delete ':username' => 'devise/registrations#destroy'
+  end
 end
